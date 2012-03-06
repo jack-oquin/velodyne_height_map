70379a7b9ec5bd6e6b9e726acbc2eb2fdff4d582
piyushk <piyushk@f2b30410-025d-11df-9f77-c71dee008b03>
2012-03-06 02:15:15 +0000

    updated calibrator code - now compiles but still untested

--- velodyne_calibration/src/nodes/calibrator.cc
+++ velodyne_calibration/src/nodes/calibrator.cc
@@ -12,6 +12,7 @@
  */
 
 #include <ros/ros.h>
+#include <ros/package.h>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <dynamic_reconfigure/server.h>
@@ -27,6 +28,7 @@
 #include <boost/thread/mutex.hpp>
 
 #define NODE "velodyne_calibrator"
+#define NUM_ROUNDS 100
 
 namespace {
   boost::shared_ptr<velodyne_rawdata::RawData> data_;
@@ -51,9 +53,14 @@ namespace {
   // Use for calibration
   bool calibrate_ = false;
   boost::mutex m_calib;
+
+  velodyne_pointcloud::Calibration default_calibration_;
+  velodyne_pointcloud::Calibration current_calibration_;
+  std::string output_file_ = "out.yaml";
 }
 
-void performCalibration(const velodyne_rawdata::VPointCloud& cloud) {
+void performCalibration(const velodyne_rawdata::VPointCloud& cloud,
+    const velodyne_msgs::VelodyneScan::ConstPtr &scan_msg) {
 
   // Compute real world x and y boundaries
   int x1, x2, y1, y2;
@@ -76,13 +83,20 @@ void performCalibration(const velodyne_rawdata::VPointCloud& cloud) {
   float y_high = distance_per_pixel_ * (image_size_ / 2 - x1); 
   float y_low = distance_per_pixel_ * (image_size_ / 2 - x2);
 
-  //ROS_INFO_STREAM(x_high << "," << x_low << "," << y_high << "," << y_low);
+  ROS_INFO_STREAM(x_high << "," << x_low << "," << y_high << "," << y_low);
+
+  // Figure out which rings have points contributing to the ground plane
+  bool calibrate_ring[velodyne_rawdata::N_LASERS];
+  for (int i = 0; i < velodyne_rawdata::N_LASERS; i++) {
+    calibrate_ring[i] = false;
+  }
 
   // Compute ground plane parameters that fit these boundaries
   pcl::PointIndices inliers;
   pcl::PointCloud<pcl::PointXYZ> plane_cloud;
   pcl::NormalEstimation<pcl::PointXYZ,pcl::Normal> estimator;
-  int i = 0;
+
+  int point_count = 0;
   BOOST_FOREACH(const velodyne_rawdata::VPoint& point, cloud.points) {
     if (point.x <= x_high && point.x >= x_low &&
         point.y <= y_high && point.y >= y_low &&
@@ -91,37 +105,97 @@ void performCalibration(const velodyne_rawdata::VPointCloud& cloud) {
       xyz_point.x = point.x;
       xyz_point.y = point.y;
       xyz_point.z = point.z;
-      inliers.indices.push_back(i++);
+      inliers.indices.push_back(point_count++);
       plane_cloud.push_back(xyz_point);
+      calibrate_ring[point.ring] = true;
     }
   }
   Eigen::Vector4f params;
   float curvature;
   estimator.computePointNormal(plane_cloud, inliers.indices, params, curvature);
 
+  // TODO print out ground plane parameters here, and the contributing rings
+
+  float sum_sq_coeff = params.x() * params.x() + 
+      params.y() * params.y() + params.z() * params.z();
+
   // Now lets work to match each ring to the plane
-  for (int ring = 0; ring < velodyne_rawdata::N_LASERS; i++) {
-    Eigen::Vector3f average_point(0,0,0);
-    int count = 0;
-    BOOST_FOREACH(const velodyne_rawdata::VPoint& point, cloud.points) {
-      if (point.ring != ring)
-        continue;
-      if (point.x <= x_high && point.x >= x_low &&
-          point.y <= y_high && point.y >= y_low &&
-          fabs(point.z - ground_height_) < 0.25) {
-        average_point.x() += point.x;
-        average_point.y() += point.y;
-        average_point.z() += point.z;
-        count++;
+  current_calibration_ = default_calibration_;
+  ros::param::set("~calibration", output_file_);
+  for (int ring = 0; ring < velodyne_rawdata::N_LASERS; ++ring) {
+
+    if (!calibrate_ring[ring])
+      continue;
+
+    ROS_INFO_STREAM("Calibrating ring " << ring);
+
+    // Search in the vicinity of the current pitch
+    float error_metric[NUM_ROUNDS];
+    for (int round = 0 ; round < NUM_ROUNDS; ++round) {
+
+      // Write new calibration
+      velodyne_pointcloud::Calibration calib = current_calibration_;
+      calib.laser_corrections[velodyne_rawdata::LASER_SEQUENCE[ring]].vert_correction =
+        current_calibration_.laser_corrections[velodyne_rawdata::LASER_SEQUENCE[ring]].vert_correction +
+        (round - NUM_ROUNDS / 2) * 0.001;
+
+      // Get the new calibration
+      calib.write(output_file_);
+      calibrated_data_.reset(new velodyne_rawdata::RawData());
+      ros::NodeHandle private_nh("~");
+      calibrated_data_->setup(private_nh);
+
+      // Get the new pointcloud
+      velodyne_rawdata::VPointCloud calibrated_cloud;
+      for (size_t i = 0; i < scan_msg->packets.size(); ++i) {
+        calibrated_data_->unpack(scan_msg->packets[i], calibrated_cloud);
       }
-    }
-    if (count != 0) {
-      average_point.x() /= count;
-      average_point.y() /= count;
-      average_point.z() /= count;
+
+      // Compute the error metric
+      int count = 0;
+      float error = 0;
+      BOOST_FOREACH(const velodyne_rawdata::VPoint& point, calibrated_cloud.points) {
+        if (point.ring != ring)
+          continue;
+        if (point.x <= x_high && point.x >= x_low &&
+            point.y <= y_high && point.y >= y_low) {
+          ++count;
+          float dist_numerator = point.x * params.x() + 
+              point.y * params.y() + point.z * params.z() + params.w();
+          error += dist_numerator * dist_numerator / sum_sq_coeff;
+        }
+      }
+      if (count != 0) {
+        error_metric[round] = error / count;
+      } else {
+        error_metric[round] = 9000.0;
+      }
+      ROS_INFO_STREAM("  round " << round << ": " << error_metric);
     }
 
+    // Compute the pitch with the lowest error
+    int best_round = 0;
+    float lowest_error = error_metric[0];
+    for (int i = 1; i < NUM_ROUNDS; i++) {
+      if (error_metric[i] < lowest_error) {
+        best_round = i;
+        lowest_error = error_metric[i];
+      }
+    }
+    ROS_INFO_STREAM("  best round: " << best_round);
+
+    // Set the current calibration to the best pitch
+    current_calibration_.laser_corrections[velodyne_rawdata::LASER_SEQUENCE[ring]].vert_correction +=
+      (best_round - NUM_ROUNDS / 2) * 0.001;
+
   }
+  current_calibration_.write(output_file_);
+
+  // Reset the current calibraion so that the image display gets updated
+  data_.reset(new velodyne_rawdata::RawData());
+  ros::NodeHandle private_nh("~");
+  data_->setup(private_nh);
+
   calibrate_ = false;
 }
 
@@ -143,7 +217,7 @@ void processScan(const velodyne_msgs::VelodyneScan::ConstPtr &scan_msg) {
 
   m_calib.lock();
   if (calibrate_) {
-    performCalibration(*cloud);
+    performCalibration(*cloud, scan_msg);
   }
   m_calib.unlock();
 
@@ -214,6 +288,12 @@ int main(int argc, char *argv[]) {
 
   ros::init(argc, argv, NODE);
   ros::NodeHandle node, private_nh("~");
+
+  // Read default calibration 
+  std::string default_calibration_file = 
+    ros::package::getPath("velodyne_pointcloud") + "tests/angles.yaml";
+  ros::param::get("~calibration", default_calibration_file);
+  default_calibration_.read(default_calibration_file);
 
   // Setup the rawdata class
   data_.reset(new velodyne_rawdata::RawData());
